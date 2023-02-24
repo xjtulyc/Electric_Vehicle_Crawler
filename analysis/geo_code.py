685ee3a94cf69a12fac6f2824207d859c9e20d35
WangRongsheng <1246321319@qq.com>
2023-02-24 19:17:25 +0800

    update

--- analysis/geo_code.py
+++ analysis/geo_code.py
@@ -1,4 +1,8 @@
 import requests
+import math
+import numpy as np
+
+CONSTANTS_RADIUS_OF_EARTH = 6371000.     # meters (m)
 
 # 注册成为开发者
 # https://lbsyun.baidu.com/apiconsole/user/choose
@@ -64,6 +68,61 @@ def Coord2Pos(lng, lat, town='true'):
     else:
         print('无法获取(%s,%s)的地理信息！' % (lat, lng))
 
+
+def GPStoXY(lat, lon, ref_lat, ref_lon):
+    # input GPS and Reference GPS in degrees
+    # output XY in meters (m) X:North Y:East
+    lat_rad = math.radians(lat)
+    lon_rad = math.radians(lon)
+    ref_lat_rad = math.radians(ref_lat)
+    ref_lon_rad = math.radians(ref_lon)
+
+    sin_lat = math.sin(lat_rad)
+    cos_lat = math.cos(lat_rad)
+    ref_sin_lat = math.sin(ref_lat_rad)
+    ref_cos_lat = math.cos(ref_lat_rad)
+
+    cos_d_lon = math.cos(lon_rad - ref_lon_rad)
+
+    arg = np.clip(ref_sin_lat * sin_lat + ref_cos_lat * cos_lat * cos_d_lon, -1.0, 1.0)
+    c = math.acos(arg)
+
+    k = 1.0
+    if abs(c) > 0:
+        k = (c / math.sin(c))
+
+    x = float(k * (ref_cos_lat * sin_lat - ref_sin_lat * cos_lat * cos_d_lon) * self.CONSTANTS_RADIUS_OF_EARTH)
+    y = float(k * cos_lat * math.sin(lon_rad - ref_lon_rad) * self.CONSTANTS_RADIUS_OF_EARTH)
+
+    return x, y
+
+
+def XYtoGPS(x, y, ref_lat, ref_lon):
+    x_rad = float(x) / self.CONSTANTS_RADIUS_OF_EARTH
+    y_rad = float(y) / self.CONSTANTS_RADIUS_OF_EARTH
+    c = math.sqrt(x_rad * x_rad + y_rad * y_rad)
+
+    ref_lat_rad = math.radians(ref_lat)
+    ref_lon_rad = math.radians(ref_lon)
+
+    ref_sin_lat = math.sin(ref_lat_rad)
+    ref_cos_lat = math.cos(ref_lat_rad)
+
+    if abs(c) > 0:
+        sin_c = math.sin(c)
+        cos_c = math.cos(c)
+
+        lat_rad = math.asin(cos_c * ref_sin_lat + (x_rad * sin_c * ref_cos_lat) / c)
+        lon_rad = (ref_lon_rad + math.atan2(y_rad * sin_c, c * ref_cos_lat * cos_c - x_rad * ref_sin_lat * sin_c))
+
+        lat = math.degrees(lat_rad)
+        lon = math.degrees(lon_rad)
+
+    else:
+        lat = math.degrees(ref_lat)
+        lon = math.degrees(ref_lon)
+
+    return lat, lon
 
 if __name__ == "__main__":
     # （1）正地理编码
